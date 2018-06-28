e230377244e102b778c64636b02b5809d2e55361
Dongliang (Larry) Yi <larry.ydl@gmail.com>
2018-06-28 10:41:17 -0500

    Update Numerical_Method_Implicit.cpp

--- Numerical_Method_Implicit/Numerical_Method_Implicit/Numerical_Method_Implicit.cpp
+++ Numerical_Method_Implicit/Numerical_Method_Implicit/Numerical_Method_Implicit.cpp
@@ -1,14 +1,10 @@
-//
 //  main.cpp
 //  Numerical_Method_Implicit
-//
 //  Created by Dongliang Yi on 4/15/16.
 //  Copyright © 2016 Dongliang Yi. All rights reserved.
-//
 
 #include <iostream>
 #include <fstream>
-
 #include "do_nm.hpp"
 
 int main(void)
@@ -25,21 +21,16 @@ int main(void)
     double T = 5; // for simplicity, I shortened the maturity. since there is no 20 year option in real world.
     
     double ** result_table2 = NULL;
-    
-    
-     result_table2 = do_euler_implicitet(N, M, r, sigma, K, S_max, T);
-     std::cout<<"The Result!\n";
-     for(int i =0; i < M; i++)
-     {
-     for(int j = 0; j<N-1; j++)
-     {
-     std::cout<<result_table2[i][j]<<" ";
-     output_file2<<result_table2[i][j]<<',';
-     }
-     std::cout<<result_table2[i][N-1]<<std::endl;
-     output_file2<<result_table2[i][N-1]<<"\n";
-     }
-    
-    
-    
+    result_table2 = do_euler_implicitet(N, M, r, sigma, K, S_max, T);
+    std::cout<<"The Result!\n";
+    for(int i =0; i < M; i++)
+    {
+         for(int j = 0; j<N-1; j++)
+        {
+            std::cout<<result_table2[i][j]<<" ";
+            output_file2<<result_table2[i][j]<<',';
+        }
+        std::cout<<result_table2[i][N-1]<<std::endl;
+        output_file2<<result_table2[i][N-1]<<"\n";
+    }
 }
